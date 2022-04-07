9184b4fa62eae01511502c18a9dbeacedcf8b235
Filipe Farias <filipe.farias.chagas@protonmail.com>
2022-04-06 23:25:09 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/edu/ifpb/dac/atividade_3/DacAtividade3Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.atividade_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacAtividade3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacAtividade3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/dac/atividade_3/DacAtividade3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.atividade_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacAtividade3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
